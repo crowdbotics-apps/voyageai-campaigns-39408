e2f40db9b2ec81f6f67ed1a6d0ebdea393344d1d
Crowdbotics <team@crowdbotics.com>
2023-03-15 20:40:44 +0000

    Initial commit.

---
+++ voyageai_campaigns_39408/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for voyageai_campaigns_39408 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voyageai_campaigns_39408.settings')
+
+application = get_wsgi_application()
